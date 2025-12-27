5f6cab1bf02e51390a26f4eb1de2f0def4f11091
Siddharth Bhat <dbgt4531@gmail.com>
2025-12-27 17:41:29 +0530

    added backend app

---
+++ tradesim-backend/src/main/java/com/siddharth/tradesim_backend/TradesimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siddharth.tradesim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesimBackendApplication.class, args);
+	}
+
+}
---
+++ tradesim-backend/src/test/java/com/siddharth/tradesim_backend/TradesimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.tradesim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
