7d3e5bb84c78219ebf150e63397d0e072407dbab
Siddharth Bhat <dbgt4531@gmail.com>
2026-01-04 04:14:10 +0530

    added integration testing if app works

--- tradesim-backend/src/test/java/com/siddharth/tradesim_backend/TradesimBackendApplicationTests.java
+++ tradesim-backend/src/test/java/com/siddharth/tradesim_backend/TradesimBackendApplicationTests.java
@@ -1,11 +1,14 @@
 package com.siddharth.tradesim_backend;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TradesimBackendApplicationTests {
 
-	// @Test
+	@Test
 	void contextLoads() {
 	}
 
