2c17fb841f718ba24cacb4a3e9275bccf33ea848
Siddharth Bhat <dbgt4531@gmail.com>
2026-01-03 16:19:41 +0530

    commented test case for future use

--- tradesim-backend/src/test/java/com/siddharth/tradesim_backend/TradesimBackendApplicationTests.java
+++ tradesim-backend/src/test/java/com/siddharth/tradesim_backend/TradesimBackendApplicationTests.java
@@ -1,14 +1,11 @@
 package com.siddharth.tradesim_backend;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("Integration test — will enable later")
 @SpringBootTest
 class TradesimBackendApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
