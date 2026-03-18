63469e5078af8ffd00357bfe1d46b47e1e8ab432
Siddharth Bhat <dbgt4531@gmail.com>
2026-03-18 22:35:28 +0530

    added schedular to check liquidity every 5 seconds

---
+++ tradesim-backend/src/main/java/com/siddharth/tradesim_backend/scheduler/LiquidationScheduler.java
@@ -0,0 +1,30 @@
+package com.siddharth.tradesim_backend.scheduler;
+
+import com.siddharth.tradesim_backend.auth.AuthRepository;
+import com.siddharth.tradesim_backend.auth.model.User;
+import com.siddharth.tradesim_backend.risk.RiskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class LiquidationScheduler {
+    private final AuthRepository authRepository;
+    private final RiskService riskService;
+
+    @Scheduled(fixedRate = 5000)
+    public void checkAllUsersForLiquidation() {
+        List<User> users = authRepository.findAll();
+
+        for (User user : users) {
+            try {
+                riskService.checkLiquidation(user);
+            } catch (Exception e) {
+                System.out.println("Liquidation check failed for user: " + user.getId());
+            }
+        }
+    }
+}
