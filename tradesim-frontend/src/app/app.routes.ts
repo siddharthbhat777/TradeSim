7308a4cf2c245b251f427e742f0d713bede89fd2
Siddharth Bhat <dbgt4531@gmail.com>
2026-06-28 04:48:41 +0530

    changed logo, added theme colors and created component as a playground

--- tradesim-frontend/src/app/app.routes.ts
+++ tradesim-frontend/src/app/app.routes.ts
@@ -14,6 +14,10 @@ export const routes: Routes = [
         loadChildren: () => import('./workspace/workspace.routes').then((module) => module.workspaceRoutes)
     },
     {
+        path: 'playground',
+        loadComponent: () => import('./testing-playground/testing-playground').then((module) => module.TestingPlayground)
+    },
+    {
         path: '**',
         redirectTo: ''
     }
---
+++ tradesim-frontend/src/app/testing-playground/testing-playground.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-playground',
+  imports: [],
+  templateUrl: './testing-playground.html',
+  styleUrl: './testing-playground.scss',
+})
+export class TestingPlayground {}
