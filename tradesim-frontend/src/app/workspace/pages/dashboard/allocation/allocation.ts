c4e3e2dd9a92bd49be9d4a11a8e006433d5dfbe4
Siddharth Bhat <dbgt4531@gmail.com>
2026-08-27 04:02:43 +0530

    made styling adjustments

--- tradesim-frontend/src/app/workspace/pages/dashboard/allocation/allocation.ts
+++ tradesim-frontend/src/app/workspace/pages/dashboard/allocation/allocation.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Card } from '../../../../shared/components/card/card';
 import { PieChartContainer } from '../../../../shared/components/charts/pie-chart-container/pie-chart-container';
 import { PieChartData } from '../../../../shared/components/charts/pie-chart-container/pie-chart/pie-chart';
+import { EmptyState } from '../../../../shared/components/empty-state/empty-state';
 
 @Component({
   selector: 'app-allocation',
-  imports: [Card, PieChartContainer],
+  imports: [Card, PieChartContainer, EmptyState],
   templateUrl: './allocation.html',
   styleUrl: './allocation.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
